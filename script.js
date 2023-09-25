6e8a18e5f6d689a9c190d8c744c3e1af8706f1d7
ameya-6964 <belvalkar7@gmail.com>
2023-09-25 22:22:27 +0530

    Added Functions ,Removing And Adding Disable Class

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Selecting DOM elements
-const mili = document.getElementById("ms");
-const sec = document.getElementById("sec");
-const min = document.getElementById("min");
-const hr = document.getElementById("hr");
-const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
-const resetButton = document.getElementById("reset");
+const millisecondDisplay = document.getElementById("ms");
+const secondDisplay = document.getElementById("sec");
+const minuteDisplay = document.getElementById("min");
+const hourDisplay = document.getElementById("hr");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
 
 // Adding event listeners
-startButton.addEventListener("click", start);
-stopButton.addEventListener("click", stop);
-resetButton.addEventListener("click", reset);
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
 
 // Variables to store time values
 let hour = 0;
@@ -21,11 +21,16 @@ let miliSecond = 0;
 // Flag to indicate if timer is running
 let isRunning = false;
 
+//This Will Add Disable Class To Stop And Reset Button
+if (isRunning === false) {
+  disableBtn();
+}
 // Start the timer
 function start() {
   isRunning = true;
-  startButton.classList.add("disabled");
-
+  startBtn.classList.add("disabled");
+  //This Will Remove Disable Class To Stop And Reset Button
+  removeDisableBtn();
   // Call the stopWatch function
   stopWatch();
 }
@@ -33,13 +38,14 @@ function start() {
 // Pause the timer
 function stop() {
   isRunning = false;
-  startButton.classList.remove("disabled");
+  startBtn.classList.remove("disabled");
 }
 
 // Reset the timer
 function reset() {
   isRunning = false;
-  startButton.classList.remove("disabled");
+  disableBtn();
+  startBtn.classList.remove("disabled");
 
   // Reset time values
   hour = 0;
@@ -85,10 +91,10 @@ function updateTimeDisplay() {
   let formattedSecond = formatNumber(second);
   let formattedMiliSecond = miliSecond;
 
-  mili.innerHTML = formattedMiliSecond;
-  sec.innerHTML = formattedSecond;
-  min.innerHTML = formattedMinute;
-  hr.innerHTML = formattedHour;
+  millisecondDisplay.innerHTML = formattedMiliSecond;
+  secondDisplay.innerHTML = formattedSecond;
+  minuteDisplay.innerHTML = formattedMinute;
+  hourDisplay.innerHTML = formattedHour;
 }
 
 // Add leading zero
@@ -97,4 +103,15 @@ function formatNumber(number) {
     return "0" + number;
   }
   return number;
+}
+
+// Function To Add Disable Class t0 Button Stop And Reset Button
+function disableBtn() {
+  stopBtn.classList.add("disabled");
+  resetBtn.classList.add("disabled");
+}
+// Function To Remove Disable Class t0 Button Stop And Reset Button
+function removeDisableBtn() {
+  stopBtn.classList.remove("disabled");
+  resetBtn.classList.remove("disabled");
 }
